ba875c19e1f0a78363704f54ce8737342bc64118
Abby Johnson <brit13@gmail.com>
2017-10-26 23:40:03 -0700

    added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,17 @@ var mysql = require('mysql');
 
 // CONNECT TO DATABASE
 // ====================================================
-var connection = mysql.createConnection({
-  host     : 'localhost', 
-  user     : 'root',
-  password : 'Bacardi13',
-  database : 'burgers_db'
-})
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host     : 'localhost', 
+    user     : 'root',
+    password : 'Bacardi13',
+    database : 'burgers_db'
+});
 
 connection.connect(function(err) {
   if (err) {
